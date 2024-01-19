0572a781ca75f1f142cdaeea6998273a2f3bb72a
ZHAYRI123 <merykozan@gmail.com>
2024-01-19 12:43:45 -0800

    update

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#Using Puppet, create a file in /tmp
+
+file { 'school':
+path    => '/tmp/school',
+owner   => 'www-data',
+group   => 'www-data',
+mode    => '0744',
+content => 'I love Puppet',
+}
